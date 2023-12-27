9c04b460ed3aaf319be1c43f0ac637fdc6356f27
Selin Namak <selin-guzel@hotmail.com>
2023-12-27 16:03:18 +0300

    ödev tamamlandı

---
+++ Assets/OdevScripti.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OdevScripti : MonoBehaviour
+{
+    static void bolenleriBul(int ilkSayi, int ikinciSayi)
+    {
+        List<int> tumListe = new List<int>();
+        String listString = "";
+
+        for (int i = ilkSayi; i <= ikinciSayi; i++)
+        {
+            tumListe.Add(i);
+        }
+
+        foreach (int sayi in tumListe)
+        {
+            listString += sayi + " - ";
+        }
+        print("Tüm Liste: " + listString);
+
+        listString = "";
+
+        for (int i = 0; i < tumListe.Count; i++)
+        {
+            if (tumListe[i] % 2 == 0)
+            {
+                listString += tumListe[i] + " - ";
+            }
+        }
+        print("İkiye bölünenler " + listString);
+
+        listString = "";
+        for (int i = 0; i < tumListe.Count; i++)
+        {
+            
+            if (tumListe[i] % 3 == 0)
+            {
+                listString += tumListe[i] + " - ";
+                
+            }
+            
+        }
+        print("Üçe bölünenler " + listString);
+        listString = "";
+        for (int i = 0; i < tumListe.Count; i++)
+        {
+
+            if (tumListe[i] % 5 == 0)
+            {
+                listString += tumListe[i] + " - ";
+
+            }
+
+        }
+        print("Beşe bölünenler " + listString);
+    }
+
+// Start is called before the first frame update
+void Start()
+    {
+        bolenleriBul(12,16);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
